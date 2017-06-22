099969edfea6e8e3428ced400d969b6ac50a8fe2
antonio <antonior92@gmail.com>
2017-06-22 17:02:50 -0300

    Working version of Byrd-Omojokun SQP.

--- ipsolver/__init__.py
+++ ipsolver/__init__.py
@@ -4,6 +4,7 @@ ipsolver
 
 from .qp_subproblem import *
 from .projections import *
+from .byrd_omojokun_sqp import *
 
 
 __all__ = [s for s in dir() if not s.startswith('_')]
--- ipsolver/byrd_omojokun_sqp.py
+++ ipsolver/byrd_omojokun_sqp.py
@@ -5,9 +5,10 @@ from __future__ import division, print_function, absolute_import
 from scipy.sparse import (issparse)
 from scipy.sparse.linalg import LinearOperator
 from .projections import projections
-from .qp_subproblem import qp_subproblem
+from .qp_subproblem import qp_subproblem, inside_box_boundaries
 import numpy as np
 from numpy.linalg import norm
+import pdb
 
 __all__ = [
     'byrd_omojokun_sqp',
@@ -18,22 +19,24 @@ def default_tr_scalling(x):
     return np.ones_like(x)
 
 
-def default_stop_criteria(xk, lambda_k, fk, gk, ck, Ak, k, trust_radiusk):
-    opt = np.linalg.norm(gk - Ak.T.dot(lambda_k))
-    const = np.linalg.norm(ck)
+def default_stop_criteria(xk, vk, fk, gkSk, ck, AkSk, k, trust_radiusk):
+    opt = norm(gkSk + AkSk.T.dot(vk))
+    const = norm(ck)
 
-    if opt < 1e-8 and const < 1e-8 and k < 1000:
-        return False
-    else:
+    if (opt < 1e-8 and const < 1e-8) or k > 1000 or trust_radiusk < 1e-12:
         return True
+    else:
+        return False
 
 
 def byrd_omojokun_sqp(f, g, H, c, A, x0,
+                      v0=None,
                       stop_criteria=default_stop_criteria,
                       tr_scaling=default_tr_scalling,
                       trust_radius0=1.0,
                       penalty0=1.0,
-                      tr_lb=None, tr_ub=None):
+                      tr_lb=None, tr_ub=None,
+                      return_all=False):
     """Solve nonlinear equality-constrained problem using trust-region SQP.
 
     Solve problem:
@@ -54,7 +57,7 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
             g(x) -> array_like, shape (n,)
     H : callable
         Lagrangian hessian:
-            H(x, lambda) -> LinearOperator (or sparse matrix or ndarray), shape (n, n)
+            H(x, v) -> LinearOperator (or sparse matrix or ndarray), shape (n, n)
     c : callable
         Equality constraint:
             c(x) -> array_like, shape (m,)
@@ -63,11 +66,14 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
             A(x) -> sparse matrix (or ndarray), shape (m, n)
     x_0 : array_like, shape (n,)
         Starting point.
+    v_0 : array_like, shape (n,)
+        Initial lagrange multipliers. By default uses least-squares lagrange
+        multipliers.
     trust_radius0: float
         Initial trust-region
     stop_criteria: callable
         Functions that returns True when stop criteria is fulfilled:
-            stop_criteria(xk, lambda_k, fk, gk, ck, Ak, k, trust_radiusk)
+            stop_criteria(xk, vk, fk, gkSk, ck, AkSk, k, trust_radiusk)
     tr_scaling : callable
         Function that return scaling for the trust region:
             tr_scaling(x) -> array_like, shape (n,)
@@ -77,6 +83,31 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
         Lower bound for step (after the scaling).
     tr_ub : array_like, shape (n,), optional
         Upper bound for step (after the scaling).
+    return_all : bool, optional
+        When ``true`` return the list of all vectors through the iterations.
+
+    Returns
+    -------
+    x : array_like, shape (n,)
+        Solution to the equality constrained problem.
+    info :
+        Dictionary containing the following:
+
+            - niter : Number of iteractions.
+            - trust_radius : Trust radius at last iteraction.
+            - v : Lagrange multipliers at the solution , shape (m,).
+            - f : Function evaluation at the solution.
+            - g : Gradient evaluation at the solution.
+            - H : Lagrangian Hessian at the solution.
+            - c : Constraints at the solution.
+            - A : Constraints jacobian at the solution.
+            - opt : Optimality is the norm of gradient of the Lagrangian
+                    ``||grad L(x, v)||``,
+                     where ``grad L(x, v) = g(x) + A(x).T v``.
+            - constr : Norm of the constraint violation ``||c(x)||``
+            - allvecs : List containing all intermediary vectors (optional).
+            - allmult : List containing all intermediary lagrange
+                        multipliers (optional).
 
     Notes
     -----
@@ -106,7 +137,7 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
 
     # Initial values
     k = 0
-    xk = x0
+    xk = np.copy(x0)
     trust_radiusk = trust_radius0
     penaltyk = penalty0
     # Compute Values
@@ -124,13 +155,22 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
         AkSk = np.multiply(Ak, Sk)
     # Get projections
     Zk, LSk, Yk = projections(AkSk)
-    # Compute least-square lagrange multipliers
-    lambda_k = -LSk.dot(gk)
+    # Set initial lagrange multipliers
+    if v0 is None:
+        # Compute least-square lagrange multipliers
+        vk = -LSk.dot(gkSk)
+    else:
+        vk = v0
+    # Store values
+    if return_all:
+        allvecs = [np.copy(xk)]
+        allmult = [np.copy(vk)]
+
     compute_Hk = True
-    while not stop_criteria(xk, lambda_k, fk, gk, ck, Ak, k, trust_radiusk):
+    while not stop_criteria(xk, vk, fk, gkSk, ck, AkSk, k, trust_radiusk):
         # Compute Lagrangian Hessian
         if compute_Hk:
-            Hk = H(xk, lambda_k)
+            Hk = H(xk, vk)
 
             # Apply scaling
             def matvec(x):
@@ -141,7 +181,7 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
                                                           Zk, Yk, ck,
                                                           trust_radiusk,
                                                           tr_lb, tr_ub)
-        # Computer update (normal + tangential steps).
+        # Compute update (normal + tangential steps).
         dk = dn + dt
 
         # Compute second order model: q = 1/2 d H d + g.T d + f.
@@ -172,47 +212,57 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
         # Compute reduction ratio
         reduction_ratio = ared/pred
 
-        # Second order correction, reference [1]_, p.892.
-        if reduction_ratio < SUFFICIENT_REDUCTION and norm(dn) <= 0.1*norm(dt):
-            # Update steps
-            y = LSk.dot(ck_next)
-            dk = dk + y
-            # Recompute pred
-            qk = 1/2*(SkHkSk.dot(dk)).dot(dk) + gkSk.T.dot(dk)
-            lk = AkSk.dot(dk)+ck
-            pred = -qk + penaltyk*(norm_ck - norm(lk))
-            # Recompute ared
-            ck_next = c(xk+dk)
-            norm_ck_next = norm(ck_next)
-            merit_function_xk_next = fk_next + penaltyk*norm_ck_next
-            ared = merit_function_xk - merit_function_xk_next
-            # Recompute reduction ratio
-            reduction_ratio = ared/pred
-
         # Reajust trust region step, formula (3.55), reference [1]_, p.892.
         if reduction_ratio >= 0.9:
             trust_radiusk = max(7*norm(dk), trust_radiusk)
         elif reduction_ratio >= 0.3:
             trust_radiusk = max(2*norm(dk), trust_radiusk)
-        elif reduction_ratio >= SUFFICIENT_REDUCTION:
-            trust_radiusk = trust_radiusk
-        else:
-            new_trust_radius = 0.5*norm(dk)
-            if new_trust_radius >= 1/2*trust_radiusk:
-                trust_radiusk = 1/2*trust_radiusk
-            elif new_trust_radius >= 1/10*trust_radiusk:
-                trust_radiusk = new_trust_radius
+        elif reduction_ratio < SUFFICIENT_REDUCTION:
+            # Second order correction, reference [1]_, p.892.
+            if norm(dn) <= 0.1*norm(dt):
+                # Update steps
+                y = Yk.dot(ck_next)
+                # Recompute ared
+                fk_soc = f(xk+dk+y)
+                ck_soc = c(xk+dk+y)
+                norm_ck_soc = norm(ck_next)
+                merit_function_xk_soc = fk_soc + penaltyk*norm_ck_soc
+                ared_soc = merit_function_xk - merit_function_xk_soc
+                # Recompute reduction ratio
+                reduction_ratio_soc = ared_soc/pred
+                if reduction_ratio_soc >= SUFFICIENT_REDUCTION and \
+                   inside_box_boundaries(dk+y, tr_lb, tr_ub):
+                    dk = dk + y
+                    fk_next = fk_soc
+                    ck_next = ck_soc
+                    norm_ck_next = norm_ck_soc
+                    reduction_ratio = reduction_ratio_soc
+                else:
+                    new_trust_radius = 0.5*norm(dk)
+                    if new_trust_radius >= 1/2*trust_radiusk:
+                        trust_radiusk = 1/2*trust_radiusk
+                    elif new_trust_radius >= 1/10*trust_radiusk:
+                        trust_radiusk = new_trust_radius
+                    else:
+                        trust_radiusk = 1/10*trust_radiusk
             else:
-                trust_radiusk = 1/10*trust_radiusk
+                new_trust_radius = 0.5*norm(dk)
+                if new_trust_radius >= 1/2*trust_radiusk:
+                    trust_radiusk = 1/2*trust_radiusk
+                elif new_trust_radius >= 1/10*trust_radiusk:
+                    trust_radiusk = new_trust_radius
+                else:
+                    trust_radiusk = 1/10*trust_radiusk
 
         # Update iteration
         k += 1
         if reduction_ratio >= SUFFICIENT_REDUCTION:
-            fk = f(xk)
+            xk += dk
+            fk = fk_next
             gk = g(xk)
-            ck = c(xk)
+            ck = ck_next
             Ak = A(xk)
-            norm_ck = norm(ck)
+            norm_ck = norm_ck_next
             # Apply scaling
             Sk = tr_scaling(xk)
             gkSk = gk*Sk
@@ -223,11 +273,23 @@ def byrd_omojokun_sqp(f, g, H, c, A, x0,
             # Get projections
             Zk, LSk, Yk = projections(AkSk)
             # Compute least-square lagrange multipliers
-            lambda_k = -LSk.dot(gk)
+            vk = -LSk.dot(gkSk)
             # Set Flag
             compute_Hk = True
         else:
             compute_Hk = False
+        # Store values
+        if return_all:
+            allvecs.append(np.copy(xk))
+            allmult.append(np.copy(vk))
 
-    return xk, lambda_k, trust_radiusk
+    opt = norm(gk + Ak.T.dot(vk))
+    constr = norm(ck)
+    info = {'niter': k, 'trust_radius': trust_radiusk, 'v': vk, 'f': fk,
+            'g': gk, 'H': Hk, 'c': ck, 'A': Ak, 'opt': opt,
+            'constr': constr}
+    if return_all:
+        info['allvecs'] = allvecs
+        info['allmult'] = allmult
 
+    return xk, info
