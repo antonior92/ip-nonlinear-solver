ebf238c390ed1ba2332eeed3626e14cb99551909
antonio <antonior92@gmail.com>
2017-08-25 16:31:08 -0300

    Remove group_columns.py

--- ipsolver/setup.py
+++ ipsolver/setup.py
@@ -5,7 +5,6 @@ def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('ipsolver', parent_package, top_path)
     config.add_subpackage('_large_scale_constrained')
-    config.add_extension('_group_columns', sources=['_group_columns.c'],)
     config.add_data_dir('tests')
     return config
 
--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
-from Cython.Build import cythonize
-import numpy
 
-ext_module = cythonize("ipsolver/_group_columns.pyx")
-ext_module[0].include_dirs = [numpy.get_include(), '.']
 
 setup(name='ip-nonlinear-solver',
       version='0.1',
       description='A trust-region interior-point method for general nonlinear programing problems.',
-      author='Antonio Horta Ribeiro',
+      author='Antonio H. Ribeiro, Nikolay Mayorov, Matt Haberland, Ralf Gommers',
       author_email='antonior92@gmail.com',
-      ext_modules=ext_module,
       packages=['ipsolver'])
